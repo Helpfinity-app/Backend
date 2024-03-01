db6ee05a13fdb1bb6932c822f2188c7e05113b2a
Fan Wang <nimadorostkar97@gmail.com>
2024-03-01 23:09:23 +0330

    add all files

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from config.settings import STATIC_ROOT, STATIC_URL, MEDIA_URL, MEDIA_ROOT
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path("redirect/", UserRedirectView.as_view(), name="redirect"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
