c35ef05f09b5e977f17a89f3ac868d9167c99415
Fan Wang <nimadorostkar97@gmail.com>
2024-06-05 18:55:24 +0330

    update

--- config/settings/common.py
+++ config/settings/common.py
@@ -212,9 +212,9 @@ REST_FRAMEWORK = {
 MAX_UPLOAD_SIZE = 5242880
 
 # CORSHEADERS CONFIGURATION
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
-CORS_ALLOWED_ORIGINS = ["http://localhost", "http://127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1"]
+ALLOWED_HOSTS = ['localhost','127.0.0.1','5.161.144.222']
+CORS_ALLOWED_ORIGINS = ["http://localhost", "http://127.0.0.1", "http://5.161.144.222"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1", "http://5.161.144.222"]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_REPLACE_HTTPS_REFERER = True
 CORS_ALLOW_CREDENTIALS = True
